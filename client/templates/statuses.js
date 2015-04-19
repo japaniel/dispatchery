9d07bbfac27ad08a2c753a4d515d0e11bf097b83
Daniel Redington <japaniel@gmail.com>
2015-04-19 22:48:53 +0000

    status dropdown now updates to current status. Added *.sw* files to gitignore

--- client/templates/statuses.js
+++ client/templates/statuses.js
@@ -1,12 +1,14 @@
 Meteor.subscribe("statuses");
 
 Template.statuses.onCreated(function() {
-  Session.set("curTech", Template.parentData(0) );
+//  Session.set("curTech", Template.parentData(0) );
 });
 
 Template.statuses.helpers({
   statuses : function() {
-    var tech = Session.get("curTech");
+//    var tech = Session.get("curTech");
+    var tech = Template.parentData(0);
+    //console.log(tech);
     return Statuses.find({status: {$not: tech.status}});
   },
 
@@ -27,12 +29,6 @@ Template.statuses.events({
     var newStatus = $(e.currentTarget).text();
     var tech = Template.parentData(0);
 
-    console.log(tech)
-    //Statuses.update(tech._id, {status: newStatus});
-    Meteor.call('updateStatus', tech, status, function (error, result) {
-      if (error) {
-        console.log(error);
-      }
-    });
+    Techs.update(tech._id, {$set: {"status": newStatus} });
   }
 });
