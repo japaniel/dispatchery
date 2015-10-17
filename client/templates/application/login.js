033f65cc5482a70747e99ba6d372b7e05bf65fa7
housewright30 <housewright30@gmail.com>
2015-10-16 20:42:24 -0600

    login V1.0  admin enabled.

--- client/templates/application/login.js
+++ client/templates/application/login.js
@@ -11,36 +11,27 @@ Template.Login.events({
         var role = document.getElementsByName("role")[0].value;
         var email = $('[name=email]').val();
         var password = $('[name=password]').val();
-        Meteor.call('makeUser', email, password, role)
-        document.getElementById("email").value = "";
-        document.getElementById("pass").value = "";
-      //   Accounts.createUser({
-      //     email: email,
-      //     password: password,
-      //     profile: {
-      //       Role: role,
-      //       Email: email
-      //     }
-      // });
+        Accounts.createUser({
+          email: email,
+          password: password,
+          profile: {
+            Role: role
+            // Email: email
+          }
+      });
       }
 });
 
 Template.Login.helpers({
+  acheck: function acheck() {
+    if (Meteor.user().profile.Role == "Admin") {
+      return true
+    }else {
+      return false
+    }
+  }
 });
 
 Accounts.onLogin(function(){
   Router.go('techsWorking')
-})
-
-Tracker.autorun(function () {
-    Meteor.subscribe("userList");
-    Meteor.subscribe("allUserData");
-});
-
-
-Template.users.helpers({
-  users: function users(){
-  return Meteor.subscribe('userList')
-}
-
 })
--- server/login.js
+++ server/login.js
@@ -1,23 +1,23 @@
-Meteor.methods({
-  makeUser: function makeUser(email, password, role){
-    Accounts.createUser({
-        email: email,
-        password: password,
-        profile: {
-          Role: role,
-          Email: email
-        }
-    });
-  },
-  allUsers: function allUsers() {
-    var ausers = Meteor.users;
-  Meteor.publish("userList", function() {
-   return ausers.find().fetch();
- })
- }
-});
-// Accounts.onCreateUser(function()
-//
-// Accounts.setPassword(userId, password)
-//
-// )
+// Meteor.methods({
+//   makeUser: function makeUser(email, password, role){
+//     Accounts.createUser({
+//         email: email,
+//         password: password,
+//         profile: {
+//           Role: role,
+//           Email: email
+//         }
+//     });
+//   },
+//   allUsers: function allUsers() {
+//     var ausers = Meteor.users;
+//   Meteor.publish("userList", function() {
+//    return ausers.find().fetch();
+//  })
+//  }
+// });
+// // Accounts.onCreateUser(function()
+// //
+// // Accounts.setPassword(userId, password)
+// //
+// // )
